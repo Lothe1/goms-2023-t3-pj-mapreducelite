6476cd3cc72b45cd87ee68ef4131c5dbee566f24
lingg <kanladaporn.sir@gmail.com>
2024-06-22 18:23:51 +0700

    Fix build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,17 @@
-fn main() {
-    tonic_build::compile_protos("proto/mapreduce.proto").unwrap();
+use std::env;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // tonic_build::compile_protos("proto/mapreduce.proto").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let proto_file = "src/proto/mapreduce.proto";
+        tonic_build::configure()
+            .build_client(true)
+            .build_server(true)
+            .out_dir(&out_dir)
+            .compile(&[proto_file], &["proto"])?;
+    Ok(())
 }
+
+
 
