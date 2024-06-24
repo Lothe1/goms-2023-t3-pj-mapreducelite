2f705e7b3f473bc61794027b6affe51fa6a0f6c0
lingg <kanladaporn.sir@gmail.com>
2024-06-25 00:20:42 +0700

    Create a WorkerArgs struct

--- src/app/mrl-worker.rs
+++ src/app/mrl-worker.rs
@@ -11,6 +11,20 @@ use std::time::Duration;
 use aws_sdk_s3::Client;
 use S3::minio;
 
+#[derive(Parser)]
+struct WorkerArgs {
+    #[clap(long)]
+    join: String,
+    #[clap(long)]
+    s3_endpoint: String,
+    #[clap(long)]
+    s3_bucket: String,
+    #[clap(long)]
+    s3_access_key: String,
+    #[clap(long)]
+    s3_secret_key: String,
+}
+
 mod mapreduce {
     tonic::include_proto!("mapreduce");
 }
@@ -19,21 +33,27 @@ use mapreduce::{WorkerRegistration, WorkerRequest, Task, JobRequest};
 use mapreduce::coordinator_client::CoordinatorClient;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     print!("Hello worker!\n");
+
     let args = Args::parse();
-    let ip = args.join;
+    let ip = args.join; 
     print!("IP to join: {}", ip);
     let mut client = CoordinatorClient::connect(format!("http://{}", ip)).await.unwrap();
 
-    let join_resp = client.register_worker(Request::new(WorkerRegistration {})).await.unwrap();
-    println!("{:?}", join_resp);
-    let s3_args = join_resp.into_inner();
+    // Register with coordinator
+    let request = Request::new(WorkerRegistration { });
+    let response = client.register_worker(request).await?;
+    println!("Worker registered: {:?}", response);
+
+    let s3_args = response.into_inner();
     let s3_ip = format!("http://{}", s3_args.args.get("ip").unwrap().clone());
     let s3_user = s3_args.args.get("user").unwrap().clone();
     let s3_pw = s3_args.args.get("pw").unwrap().clone();
     // println!("s3: {} {} {}\n", s3_ip, s3_user, s3_pw);
-    let s3_client = minio::get_min_io_client(s3_ip, s3_user, s3_pw).await.unwrap();
+
+    // Initialize S3 client
+    let s3_client = minio::get_min_io_client(s3_ip.clone(), s3_user.clone(), s3_pw.clone()).await?;
     let resp = s3_client.list_buckets().send().await.unwrap();
 
     // println!("bucket accessible: {}", minio::is_bucket_accessible(s3_client.clone(), bucket_name.to_string()).await.unwrap());
