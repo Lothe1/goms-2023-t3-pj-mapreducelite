e0aded2213974ad781a9d5f26e7d8184ce3d2c8f
Tath Kanchanarin <tath9982@gmail.com>
2024-06-27 02:58:57 +0700

    Implemented batch reading to facilate worker

--- src/Encode/encode_decode.rs
+++ src/Encode/encode_decode.rs
@@ -12,18 +12,53 @@ use parquet::file::properties::WriterProperties;
 use bytes::Bytes;
 
 
-
 // fn main() {
 //         let key = vec![Bytes::from("ball"), Bytes::from("bat"), Bytes::from("glove"), Bytes::from("glove")];
 //         let value = vec![Bytes::from("ayo huh"), Bytes::from("ayo 121huh"), Bytes::from("ayuh"), Bytes::from("ayh")];
-//         write_parquet(key, value);
-//         let res = read_parquet();
-//         println!("Key: {:?}", res.0);
-//         println!("Value: {:?}", res.1);
+//         let key2 = vec![Bytes::from("O promised consort"), Bytes::from("let"), Bytes::from("us"), Bytes::from("go")];
+//         let value2 = vec![Bytes::from("If you grieve"), Bytes::from("for this world"), Bytes::from("yield the path"), Bytes::from("forward to us")];
+//         write_parquet("output.parquet", key, value);
+//         write_parquet("output2.parquet", key2, value2);
+//         let res = read_parquet("output.parquet");
+//         let res2 = read_parquet("output2.parquet");
+//         combine_parquets(vec!["output.parquet", "output2.parquet"], "output_combined.parquet");
+//         let res3 = read_parquet("output_combined.parquet");
+//         let res4 = batch_reading_parquet("output_combined.parquet", 2, 20);
+//         // println!("Read from parquet: {:?}", res);
+//         // println!("Read from parquet: {:?}", res2);
+//         println!("Read from parquet: {:?}", res3);
+//         println!("Read from parquet: {:?}", res4);
+//
 //
 // }
 
 
+
+
+
+// Will return from start to start + batch_size, if batch size exceeds the number of rows, it will return the rest of the rows
+fn batch_reading_parquet(filename: &str, start: usize, batch_size: usize) -> (Vec<Bytes>, Vec<Bytes>){
+        let file = File::open(filename).unwrap();
+        let builder = ParquetRecordBatchReaderBuilder::try_new(file).unwrap();
+        let mut reader = builder.build().unwrap();
+        let mut ret_key = Vec::new();
+        let mut ret_value = Vec::new();
+        let record_batch = reader.next().unwrap().unwrap();
+        let row_size = record_batch.num_rows();
+        let actual_batch_size = if start + batch_size > row_size {
+                row_size - start
+        } else {
+                batch_size
+        };
+        let key_arr_ref = record_batch.column(0).slice(start, actual_batch_size);
+        let value_arr_ref = record_batch.column(1).slice(start, actual_batch_size);
+        let key = arr_to_vec(key_arr_ref.as_any().downcast_ref::<BinaryArray>().unwrap());
+        let value = arr_to_vec(value_arr_ref.as_any().downcast_ref::<BinaryArray>().unwrap());
+        ret_key.extend(key);
+        ret_value.extend(value);
+        return (ret_key, ret_value);
+
+}
 fn write_parquet(filename:&str, key: Vec<Bytes>, value: Vec<Bytes>){
         let file = File::create(filename).unwrap();
 
