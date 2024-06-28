5addf969b746e8ca37a1ab52d38228a80b292b33
Tath Kanchanarin <tath9982@gmail.com>
2024-06-28 18:24:36 +0700

    Implemented upload parts for big files

--- src/Encode/encode_decode.rs
+++ src/Encode/encode_decode.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::path::Path;
 use std::sync::Arc;
 use parquet::arrow::ArrowWriter;
 use arrow::array::{Int32Array, ArrayRef, PrimitiveArray, Array};
@@ -6,11 +7,21 @@ use arrow::array::{Int32Array, ArrayRef, PrimitiveArray, Array};
 use arrow::array::BinaryArray;
 use arrow::datatypes::{DataType, Field, Schema, SchemaRef};
 use arrow::record_batch::RecordBatch;
+use aws_sdk_s3::Client;
+use aws_sdk_s3::operation::create_multipart_upload::CreateMultipartUploadOutput;
+use aws_sdk_s3::types::{CompletedMultipartUpload, CompletedPart};
 use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 use parquet::basic::Compression;
 use parquet::file::properties::WriterProperties;
 use bytes::Bytes;
+use std::error::Error;
+use std::io::prelude::*;
+use aws_smithy_types::byte_stream::{ByteStream, Length};
 
+
+use std::process;
+use aws_sdk_s3::config::{Builder, Credentials};
+use uuid::Uuid;
 
 // fn main() {
 //         let key = vec![Bytes::from("ball"), Bytes::from("bat"), Bytes::from("glove"), Bytes::from("glove")];
@@ -153,4 +164,106 @@ fn combine_parquets(input_files: Vec<&str>, output_file: &str) -> (){
         }
 
         writer.close().unwrap();
+}
+
+
+//filename has to be the same name as upload to s3
+pub async fn upload_parts(client: &Client, bucket: &str, filename: &str)-> Result<(), Box<dyn std::error::Error>> {
+        //Split into 5MB chunks
+        const CHUNK_SIZE: u64 = 1024 * 1024 * 5;
+        const MAX_CHUNKS: u64 = 10000;
+
+        let destination_filename = filename;
+        let multipart_upload_res: CreateMultipartUploadOutput = client
+            .create_multipart_upload()
+            .bucket(bucket)
+            .key(destination_filename)
+            .send()
+            .await
+            .unwrap();
+
+        println!("Created multipart upload with ID: {}", multipart_upload_res.upload_id.as_ref().unwrap());
+
+
+        let upload_id = multipart_upload_res.upload_id.unwrap();
+
+        let path = Path::new(filename);
+        let file_size = tokio::fs::metadata(path)
+            .await
+            .expect("it exists I swear")
+            .len();
+
+        let mut chunk_count = (file_size / CHUNK_SIZE) + 1;
+        let mut size_of_last_chunk = file_size % CHUNK_SIZE;
+        if size_of_last_chunk == 0 {
+                size_of_last_chunk = CHUNK_SIZE;
+                chunk_count -= 1;
+        }
+
+        if file_size == 0 {
+                panic!("Bad file size.");
+        }
+        if chunk_count > MAX_CHUNKS {
+                panic!("Too many chunks! Try increasing your chunk size.")
+        }
+
+        let mut upload_parts: Vec<CompletedPart> = Vec::new();
+
+        for chunk_index in 0..chunk_count {
+                let this_chunk = if chunk_count - 1 == chunk_index {
+                        size_of_last_chunk
+                } else {
+                        CHUNK_SIZE
+                };
+                let stream = ByteStream::read_from()
+                    .path(path)
+                    .offset(chunk_index * CHUNK_SIZE)
+                    .length(Length::Exact(this_chunk))
+                    .build()
+                    .await
+                    .unwrap();
+                //Chunk index needs to start at 0, but part numbers start at 1.
+                let part_number = (chunk_index as i32) + 1;
+                // snippet-start:[rust.example_code.s3.upload_part]
+                let upload_part_res = client
+                    .upload_part()
+                    .key(filename)
+                    .bucket(bucket)
+                    .upload_id(upload_id.clone())
+                    .body(stream)
+                    .part_number(part_number)
+                    .send()
+                    .await;
+
+
+
+                upload_parts.push(
+                        CompletedPart::builder()
+                            .e_tag(upload_part_res.unwrap().e_tag.unwrap_or_default())
+                            .part_number(part_number)
+                            .build(),
+                );
+                // snippet-end:[rust.example_code.s3.upload_part]
+        }
+
+
+        // snippet-start:[rust.example_code.s3.upload_part.CompletedMultipartUpload]
+        let completed_multipart_upload: CompletedMultipartUpload = CompletedMultipartUpload::builder()
+            .set_parts(Some(upload_parts))
+            .build();
+        // snippet-end:[rust.example_code.s3.upload_part.CompletedMultipartUpload]
+
+        // snippet-start:[rust.example_code.s3.complete_multipart_upload]
+        let _complete_multipart_upload_res = client
+            .complete_multipart_upload()
+            .bucket(bucket)
+            .key(filename)
+            .multipart_upload(completed_multipart_upload)
+            .upload_id(upload_id.clone())
+            .send()
+            .await
+            .unwrap();
+        // snippet-end:[rust.example_code.s3.complete_multipart_upload]
+
+        Ok(())
 }
